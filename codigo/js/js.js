a64be92216e9883e59b19d1d44dc68af592bff43
Aiub <aiubraissuni@gmail.com>
2024-10-30 10:35:04 +0100

    funcion cargar

--- codigo/js/js.js
+++ codigo/js/js.js
@@ -9,8 +9,6 @@ let imgJugadorA = "../../imagenes/x.jpg";
 let imgJugadorB = "../../imagenes/o.jpg";
 
 function cargar() {
-  // Inicio del jugador
-  inicioJugador();
 
   let fichas = Array.from(document.querySelectorAll("[draggable='true']"));
   let casillas = Array.from(document.querySelectorAll("#tabla td"));
@@ -49,108 +47,66 @@ function cargar() {
         } else {
           //abrirVentanaEmergente("Error: Ficha no encontrada");
         }
-  let fichas = Array.from(document.querySelectorAll("[draggable]"));
-  let casillas = Array.from(document.getElementsByTagName("td"));
-
-  fichas.forEach((item) => {
-    item.addEventListener("dragstart", dragstart);
-  });
-
-  casillas.forEach((item) => {
-    item.addEventListener("dragenter", function (event) {
-      event.preventDefault();
-    });
-
-    item.addEventListener("dragover", function (event) {
-      event.preventDefault();
-    });
-
-    item.addEventListener("dragleave", function (event) {
-      event.preventDefault();
-    });
-
-    item.addEventListener("drop", function (event) {
-      event.preventDefault();
-      // Comprobar si la casilla ya tiene un hijo
-      if (!event.target.hasChildNodes()) {
-        event.target.appendChild(
-          document.getElementById(event.dataTransfer.getData("text/plain"))
-        );
-        turnoJugador();
-        cambiarImagenTurno();
       } else {
         abrirVentanaEmergente("Error");
       }
     });
   });
 
-  function dragstart(e) {
-    e.dataTransfer.setData("text/plain", e.target.id);
-  }
-
-  function cambiarImagenTurno() {
-    if (jugadorActual == jugadorA) {
-      divImagenTurno.innerHTML = `<img src="${imgJugadorA}" alt="${jugadorA}">`;
-    } else {
-      divImagenTurno.innerHTML = `<img src="${imgJugadorB}" alt="${jugadorB}">`;
-    }
+  function dragstart(e){
+      e.dataTransfer.setData('text/plain',e.target.id);
   }
 
   // Determinar turno del jugador, comienzan los rojos
   // Determinar turno del jugador de inicio
-  function inicioJugador() {
-    // Rndom
-    let jugadorEmpieza = Math.floor(Math.random() * 2) + 1;
+  let jugadorEmpieza = Math.floor(Math.random() * 2) + 1;
 
-    // Obtengo div del turno jugador
+  // Obtengo div del turno jugador
 
-    let divImagenTurno = document.getElementById("turnoJugador");
+  let divImagenTurno = document.getElementById("turnoJugador");
 
-    // Si es par empieza el jugador A
-    if (jugadorEmpieza % 2 == 0) {
-      jugadorActual = jugadorA;
+  alert(divImagenTurno);
 
-      // Asigno en el div el siguiente contenido
-      divImagenTurno.innerHTML = `<img src="${imgJugadorA}" alt="${jugadorA}">`;
-    } else {
-      jugadorActual = jugadorB;
-      divImagenTurno.innerHTML = `<img src="${imgJugadorB}" alt="${jugadorB}">`;
-    }
+  // Si es par empieza el jugador A
+  if (jugadorEmpieza % 2 == 0) {
+    jugadorActual = jugadorA;
+
+    // Asigno en el div el siguiente contenido
+    divImagenTurno.innerHTML = `<img src="${imgJugadorA}" alt="${jugadorA}">`;
+  } else {
+    jugadorActual = jugadorB;
+    divImagenTurno.innerHTML = `<img src="${imgJugadorB}" alt="${jugadorB}">`;
   }
 
   function turnoJugador() {
     // Si hay un ganador(TRUE), llamamos actualizarMarcador
-    if (determinarGanador() != false) {
+    if (determinarGanador()) {
     } else {
-      if (jugadorActual == jugadorA) {
-        jugadorActual = jugadorB;
-      } else {
-        jugadorActual = jugadorA;
-      }
+      // Cambiamos de turno
     }
   }
 
   // Método para determinar el ganador
 
   const determinarGanador = () => {
-    let casillas = document.querySelectorAll("td");
 
+    let casillas=document.querySelectorAll("td");
+    
     // Definir las combinaciones ganadoras del tablero
-    const combinacionesGanadoras = [
-      [0, 1, 2], // Filas
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6], // Columnas
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8], // Diagonales
-      [2, 4, 6],
-    ];
-    // Revisar cada combinación ganadora
-    for (let i = 0; i < combinacionesGanadoras.length; i++) {
-      const [a, b, c] = combinacionesGanadoras[i];
+  const combinacionesGanadoras = [
+    [0, 1, 2], // Filas
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], // Columnas
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8], // Diagonales
+    [2, 4, 6]
+  ];
+  // Revisar cada combinación ganadora
+  for (let i = 0; i < combinacionesGanadoras.length; i++) {
+    const [a, b, c] = combinacionesGanadoras[i];
 
-<<<<<<< HEAD
     if (
       casillas[a].className &&
       casillas[a].className === casillas[b].className &&
@@ -158,29 +114,22 @@ function cargar() {
     ) {
       return casillas[a].className; // Retorna 'X' o 'O' según el ganador
       
-=======
-      if (
-        casillas[a].className &&
-        casillas[a].className === casillas[b].className &&
-        casillas[a].className === casillas[c].className
-      ) {
-        return casillas[a].className; // Retorna 'X' o 'O' según el ganador
-      }
->>>>>>> beeb9fccbce0eb0d80b01678df6de7bddfaab210
     }
-    return false;
+  }
+
   };
 
   //Metodo actualizar marcador
   const actualizarMarcador = () => {
-    let casillas = document.querySelectorAll("td");
+    let casillas=document.querySelectorAll("td");
     let ganador = determinarGanador();
-
-    if (ganador === "x") {
+  
+    if (ganador ==="x") {
       victoriasA++;
     } else if (ganador === "o") {
       victoriasB++;
     }
+  
 
     let marcadorA = document.getElementById("victoriasA");
     let marcadorB = document.getElementById("victoriasB");
@@ -191,21 +140,20 @@ function cargar() {
 
   // Reiniciar el juego
 
-  // Pantalla error, no borrar el codigo por favor
+  // Pantalla error
 
-  function abrirVentanaEmergente(mensaje) {
-    let idError = setTimeout(function () {
-      // Abre la página de Google en una nueva ventana de 500x500px
+  /*
+  let idError = setTimeout(function () {
+    // Abre la página de Google en una nueva ventana de 500x500px
 
-      let ventanaError = window.open(
-        "about:blank",
-        "_blank",
-        "width=200px,height=100px,left=50px,top=50px" // Desde la ventana
-      );
+    let ventanaError = window.open(
+      "about:blank",
+      "_blank",
+      "width=200px,height=100px,left=50px,top=50px" // Desde la ventana
+    );
 
-      switch (mensaje) {
-        case "Error":
-          ventanaError.document.write(`
+    if (ventanaError) {
+      ventanaError.document.write(`
        <html>
         <head>
           <title>Error</title>
@@ -214,42 +162,8 @@ function cargar() {
         </body>
       </html>
       `);
-
-          // LLamada al método para que vuelva la ficha a su posición de origen
-          break;
-
-        case "Turno":
-          ventanaError.document.write(`
-       <html>
-        <head>
-          <title>Error</title>
-            <body>
-          <p>TURNO INCORRECTO.</p>
-        </body>
-      </html>
-      `);
-
-          break;
-        case "Ganador":
-          ventanaError.document.write(`
-       <html>
-        <head>
-          <title>Error</title>
-            <body>
-          <p>GANADOR JUGADOR X.</p>
-        </body>
-      </html>
-      `);
-          break;
-
-        default:
-          break;
-      }
-
-      if (ventanaError) {
-      }
-    }, 1000); // Duración de 3 segundos
-  }
-
-  // Función F5
+    }
+  }, 1000); // Duración de 3 segundos
+*/
+  // Pantalla ganador
 }
